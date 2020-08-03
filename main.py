1d85ec36cc886f345da79374f7d5ea1a06d89c30
Saksham Johri <sakshamjohri@gmail.com>
2020-08-03 19:39:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 !HealthyProgrammer
-*Reminds the Pogrammer to Drink Water, Do Eyes Exercise, and to Do Physical Exercise at certain Time Interwals
+*Reminds the Pogrammer to Drink Water, Do Eyes Exercise, and to Do Physical Exercise at certain Time Intervals
 """
 
 from pygame import mixer
